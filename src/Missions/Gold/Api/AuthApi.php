0c94e284b823fdba07bfc08fdac7a8bbfbc68b55
Gergely Laborci <gergely@laborci.hu>
2021-09-11 17:19:23 +0200

    fix

--- src/Missions/Gold/Api/AuthApi.php
+++ src/Missions/Gold/Api/AuthApi.php
@@ -7,37 +7,37 @@ use Atomino\Mercury\Responder\Api\Attributes\Auth;
 use Atomino\Mercury\Responder\Api\Attributes\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class AuthApi extends Api{
+class AuthApi extends Api {
 
-	public function __construct(private SessionAuthenticator $authenticator){ }
+	public function __construct(private SessionAuthenticator $authenticator) { }
 
-	#[Route( self::POST, '/get' )]
-	public function get(){
+	#[Route(self::POST, '/get')]
+	public function get() {
 		$user = User::getAuthenticated();
-		if(is_null($user)) return null;
+		if (is_null($user)) return null;
 		$roles = $user->getRoles();
 		$name = $user->name;
 		return [
-			"name"=>$name,
-			"roles"=>$roles
+			"name"  => $name,
+			"roles" => $roles,
 		];
 	}
 
-	#[Route( self::POST, '/login' )]
-	#[Auth( 'admin' )]
-	public function login(){
+	#[Route(self::POST, '/login')]
+	#[Auth(false)]
+	public function login() {
 		$login = $this->data->get('login');
 		$password = $this->data->get('password');
-		if (!$this->authenticator->login($login, $password)){
+		if (!$this->authenticator->login($login, $password)) {
 			$this->setStatusCode(Response::HTTP_UNAUTHORIZED);
 			return false;
 		}
 		return true;
 	}
 
-	#[Route( self::POST, '/logout' )]
+	#[Route(self::POST, '/logout')]
 	#[Auth]
-	public function logout(){
+	public function logout() {
 		$this->authenticator->logout($this->getResponse());
 	}
 
